078bb3d9a49901eff259765755410c56eeaae631
CarlosC <caloca@ubilogix.com>
2014-09-18 00:24:05 -0700

    Administracion fix for cut contenido

--- app/views/administracion/head.blade.php
+++ app/views/administracion/head.blade.php
@@ -28,3 +28,11 @@
           <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
           <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
         <![endif]-->
+		
+		<style>
+		@media (min-width: 1200px) {
+			.container {
+				width: 970px;
+			}
+		}
+		</style>
