53c2a22304002ea1f58817e844cefe420ff52f18
CarlosC <caloca@ubilogix.com>
2014-09-28 23:51:07 -0700

    Quite Hola mundo de index

--- app/views/index/index.blade.php
+++ app/views/index/index.blade.php
@@ -8,7 +8,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0,maximum-scale = 1.0, user-scalable=0">
-<title>Todo Construimos HOLA MUNDO</title>
+<title>Todo Construimos</title>
 <link href="/index/css/bootstrap.css" rel='stylesheet' type='text/css' />
 <!--<link href="/index/css/bootstrap.min.css" rel='stylesheet' type='text/css' />
 <script src="/index/js/bootstrap.min.js"></script>-->
