00ec27b63dfd04a7a32b8a1b8b4102e521023579
CarlosC <caloca@ubilogix.com>
2014-10-24 13:53:42 -0700

    Fix de mayusculas en anuncios

--- app/views/index/include_anuncios.blade.php
+++ app/views/index/include_anuncios.blade.php
@@ -3,7 +3,7 @@
                              <div class="banner-wrap">
                                  <div class="slider-left">
                                     <h2></h2>
-                                    <p class="top_desc">{{$anuncio->anuncio}}</p>
+                                    <p class="top_desc" style="padding-top: 5px;">{{mb_strtoupper($anuncio->anuncio, 'utf-8')}}</p>
                                     <p class="bottom_desc">{{$anuncio->periodo}}</p>
                                  </div>
                              </div>
